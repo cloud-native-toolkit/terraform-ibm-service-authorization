4f52554a74c7e6bc4fe465de757d56531aa931c3
Sean Sundberg <seansund@us.ibm.com>
2021-05-09 10:40:09 -0500

    Renames output to id
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 # Copyright 2021 IBM
 #####################################################
 
-output "iam_authorization_policy_id" {
+output "id" {
   description = "The ID of the authorization policy ID"
   value       = var.provision ? ibm_iam_authorization_policy.policy[0].id : ""
 }
