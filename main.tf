48eb818a35a09bb84b02992745053051effc9e66
Sean Sundberg <seansund@us.ibm.com>
2021-09-08 23:52:36 -0500

    Adds flags for service_instance and target_instance (#7)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -4,14 +4,19 @@
 # Copyright 2021 IBM
 #####################################################
 
+locals {
+  source_resource_instance_id = var.source_instance ? var.source_resource_instance_id : null
+  target_resource_instance_id = var.target_instance ? var.target_resource_instance_id : null
+}
+
 resource ibm_iam_authorization_policy policy {
   count = var.provision ? 1 : 0
 
   source_service_name         = var.source_service_name
   target_service_name         = var.target_service_name
   roles                       = var.roles
-  source_resource_instance_id = var.source_resource_instance_id
-  target_resource_instance_id = var.target_resource_instance_id
+  source_resource_instance_id = local.source_resource_instance_id
+  target_resource_instance_id = local.target_resource_instance_id
   source_resource_group_id    = var.source_resource_group_id
   target_resource_group_id    = var.target_resource_group_id
   source_resource_type        = var.source_resource_type
