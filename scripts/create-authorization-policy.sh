53a4560d54914f015135ce66725c219ad5a3387d
Andrew Trice <amtrice@us.ibm.com>
2022-01-11 10:20:48 -0500

    changed ibmcloud cli login b/c it breaks inside the git action
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-authorization-policy.sh
+++ scripts/create-authorization-policy.sh
@@ -3,7 +3,7 @@
 PATH=$BIN_DIR:$PATH
 
 ibmcloud api cloud.ibm.com
-ibmcloud login -q --apikey ${IBMCLOUD_API_KEY}
+ibmcloud login -q --apikey ${IBMCLOUD_API_KEY} --no-region
 ibmcloud target -r $REGION
 
 
