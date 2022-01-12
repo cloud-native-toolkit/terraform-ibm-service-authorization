cdc989f628c62c154923401404e759022dd7ab05
Andrew Trice <amtrice@us.ibm.com>
2022-01-12 09:39:35 -0500

    removed hard coded  jq path
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-authorization-policy.sh
+++ scripts/create-authorization-policy.sh
@@ -3,8 +3,6 @@
 PATH=$BIN_DIR:$PATH
 JQ=$(command -v jq | command -v ./bin/jq)
 
-JQ="/usr/local/bin/jq" 
-
 IAM_TOKEN=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${IBMCLOUD_API_KEY}" | ${JQ} -r '.access_token')
